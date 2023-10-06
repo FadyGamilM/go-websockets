26e8bbf1c9ef31626721e2c0366024fefdd54514
[FadyGamilM] <1700961@eng.asu.edu.eg>
2023-10-06 10:16:33 +0300

    feature/support the heartbeat feature so server can catch a disconnected client

--- README.md
+++ README.md
@@ -8,3 +8,19 @@
 The websocket connection of gorillamux actually allows one concurrent writer at a time 
 
 browser actually responds to the ping messages by default, all we need to do is to handle the server-side code 
+-> this what happend when a client connected and suddenly disconnected, the server will now by catching the "writemsg: websocket: close sent"
+```cmd
+2023/10/06 10:11:32 [Heartbeat] | ping
+2023/10/06 10:11:32 pong
+2023/10/06 10:11:41 [Heartbeat] | ping
+2023/10/06 10:11:41 pong
+2023/10/06 10:11:50 [Heartbeat] | ping
+2023/10/06 10:11:50 pong
+2023/10/06 10:11:59 [Heartbeat] | ping
+2023/10/06 10:11:59 pong
+2023/10/06 10:12:08 [Heartbeat] | ping
+2023/10/06 10:12:08 pong
+2023/10/06 10:12:15 error ==> websocket: close 1000 (normal)
+2023/10/06 10:12:17 [Heartbeat] | ping
+2023/10/06 10:12:17 writemsg:  websocket: close sent
+```
--- internal/transport/ws/ws-client.go
+++ internal/transport/ws/ws-client.go
@@ -3,8 +3,18 @@ package ws
 import (
 	"encoding/json"
 	"log"
+	"time"
 
 	"github.com/gorilla/websocket"
+)
+
+var (
+	// pongWait is how long we will await a pong response from client
+	pongWait = 10 * time.Second
+	// pingInterval has to be less than pongWait, We cant multiply by 0.9 to get 90% of time
+	// Because that can make decimals, so instead *9 / 10 to get 90%
+	// The reason why it has to be less than PingRequency is becuase otherwise it will send a new Ping before getting response
+	pingInterval = (pongWait * 9) / 10
 )
 
 type Client struct {
@@ -29,6 +39,16 @@ func (c *Client) ReadMessages() {
 		// remove client will remove the client and close its connection
 		c.Manager.RemoveClient(c)
 	}()
+
+	// Configure Wait time for Pong response, use Current time + pongWait
+	// This has to be done here to set the first initial timer.
+	if err := c.Conn.SetReadDeadline(time.Now().Add(pongWait)); err != nil {
+		log.Println(err)
+		return
+	}
+	// Configure how to handle Pong responses when we recieve a pong from the client
+	c.Conn.SetPongHandler(c.pongHandler)
+
 	for {
 		_, msgPayload, err := c.Conn.ReadMessage()
 		if err != nil {
@@ -67,7 +87,13 @@ func (c *Client) ReadMessages() {
 }
 
 func (c *Client) WriteMessages() {
+	// i defined this ticker and the hearbeat logic in the writeMessage because we need to ping (send) to the client and this is the role of the write message method
+	// define a ticker to ping on a ping interval rate defined previously which is 90% of the pong-wait time
+	heartbeatTicker := time.NewTicker(pingInterval)
 	defer func() {
+		// close the ticker to free the reousrce
+		heartbeatTicker.Stop()
+		// remove the client and close its connection to clean reousrces
 		c.Manager.RemoveClient(c)
 	}()
 	for {
@@ -93,6 +119,21 @@ func (c *Client) WriteMessages() {
 			}
 			// we succedded
 			log.Println("message send ..")
+		case <-heartbeatTicker.C:
+			// ping the client
+			log.Println("[Heartbeat] | ping")
+			// Send the Ping
+			if err := c.Conn.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+				log.Println("[Heartbeat] | the connection is closed via the client : ", err)
+				return // return to break this goroutine triggeing cleanup
+			}
 		}
 	}
+}
+
+// pongHandler is used to handle PongMessages for the Client
+func (c *Client) pongHandler(pongMsg string) error {
+	// Current time + Pong Wait time
+	log.Println("[Heartbeat] | pong")
+	return c.Conn.SetReadDeadline(time.Now().Add(pongWait))
 }
